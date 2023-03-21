e161932c5ff45783224c0482b9dd56a791a61823
edwin <edwinkun@gmail.com>
2023-03-21 22:56:10 +0700

    removing resource server :snail:

--- src/main/java/com/edw/config/SecurityConfiguration.java
+++ src/main/java/com/edw/config/SecurityConfiguration.java
@@ -1,16 +1,15 @@
 package com.edw.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * <pre>
@@ -24,38 +23,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Autowired
-    private KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(keycloakJwtAuthenticationConverter);
-
-        http.oauth2Login().userInfoEndpoint();
-
-        http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .oauth2Client()
+                    .and()
+                .oauth2Login()
+                .tokenEndpoint()
+                    .and()
+                .userInfoEndpoint()
+                .userAuthoritiesMapper(userAuthoritiesMapper());
         http
                 .authorizeHttpRequests()
-                            .requestMatchers("/unauthenticated").permitAll()
-                            .requestMatchers("/oauth2/**").permitAll()
+                            .requestMatchers("/unauthenticated", "/oauth2/**", "/login/**").permitAll()
                             .anyRequest()
-                                .authenticated();
+                                .fullyAuthenticated()
+                .and()
+                    .logout()
+                    .logoutSuccessUrl("http://localhost:8080/realms/external/protocol/openid-connect/logout?redirect_uri=http://localhost:8081/");
 
         return http.build();
     }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("admin")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-        return new InMemoryUserDetailsManager(user);
+    private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            return mappedAuthorities;
+        };
     }
 }
--- src/main/java/com/edw/controller/IndexController.java
+++ src/main/java/com/edw/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.edw.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
